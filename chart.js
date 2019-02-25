48c1fd9e2a47b8ebcd646d8e48c6a6ae529807d7
Victor Borshchov <victor.borshchov@gmail.com>
2019-02-25 23:01:28 +0200

    add Pie chart

--- chart.js
+++ chart.js
@@ -27,6 +27,25 @@ function calcStatsPerDay(reloadStats) {
     return result;
 }
 
+function prepareValues(data) {
+    let values = {
+        legendData: [],
+        seriesData: [],
+        selected: {}
+    };
+    let totalReloads = 0
+    for (let key in data) {
+        totalReloads += data[key].length
+    }
+    for (let key in data) {
+        values.legendData.push(key)
+        values.seriesData.push({name: key, value: data[key].length})
+        values.selected[key] = ((data[key].length / totalReloads) * 100) > 1
+    }
+
+
+    return values;
+}
 // function groupweek(date, index, array) {
 //     d = new Date(date);
 //     d = Math.floor(d.getTime()/(1000*60*60*24*7));
@@ -72,76 +91,67 @@ chrome.storage.local.get(['reloadStats'], function(result) {
         }
     ]
 
-    const option = {
+    const legend = {
+        type: 'scroll',
+        orient: 'vertical',
+        right: 0,
+        data: dataLegend,
+        itemGap: 1,
+        itemHeight: 10,
+        itemWidth: 10,
+        bottom: 70
+    }
+
+    const toolbox = {
+        right: 5,
+        bottom: 20,
+        show : true,
+        feature : {
+            mark : {
+                show: true
+            },
+            dataView : {
+                show: true,
+                readOnly: false,
+                title: 'data view',
+                lang: ['data view', 'cancel', 'save']
+            },
+            magicType : {
+                show: false,
+                type: ['pie', 'bar', 'stack', 'tiled']
+            },
+            restore : {
+                show: true,
+                title: 'restore'
+            },
+            saveAsImage : {
+                show: true,
+                title: 'save'
+            },
+            dataZoom: {
+                show: false,
+                yAxisIndex: false,
+                title: {
+                    back: 'back',
+                    zoom: 'zoom'
+                }
+            },
+            saveAsImage: {
+                title: 'export',
+                pixelRatio: 2
+            }
+        }
+    }
+
+    const optionsBar = {
         title: {
             text: 'Reload Counts per page'
         },
         grid: {
             right: 200
         },
-        legend: {
-            type: 'scroll',
-            orient: 'vertical',
-            right: 0,
-            data: dataLegend,
-            itemGap: 1,
-            itemHeight: 10,
-            itemWidth: 10,
-            bottom: 50
-        },
-        // toolbox: {
-        //     y: 'bottom',
-        //     right: 60,
-        //     feature: {
-        //         magicType: {
-        //             type: ['stack', 'tiled']
-        //         },
-        //         dataView: {},
-        //         saveAsImage: {
-        //             pixelRatio: 2
-        //         }
-        //     }
-        // },
-        toolbox: {
-            right: 5,
-            bottom: 20,
-            show : true,
-            feature : {
-                mark : {
-                    show: true
-                },
-                dataView : {
-                    show: true,
-                    readOnly: false,
-                    title: 'data view',
-                    lang: ['data view', 'cancel', 'save']
-                },
-                magicType : {
-                    show: false,
-                    type: ['pie', 'bar', 'stack', 'tiled']
-                },
-                restore : {
-                    show: true,
-                    title: 'restore'
-                },
-                saveAsImage : {
-                    show: true,
-                    title: 'save'
-                },
-                dataZoom: {
-                    show: false,
-                    yAxisIndex: false,
-                    title: {
-                        back: 'back',
-                        zoom: 'zoom'
-                    }
-                },
-                saveAsImage: {
-                    title: 'export',
-                    pixelRatio: 2
-                }
-            }
-        },
+        legend,
+        toolbox,
         dataZoom: [
             {
                 type: 'slider',
@@ -167,6 +177,52 @@ chrome.storage.local.get(['reloadStats'], function(result) {
         }
     };
 
+    const barData = prepareValues(result.reloadStats)
+
+    const optionsPie = {
+        title : {
+            text: 'Reload Counts per page',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{b} : {c} ({d}%)"
+        },
+        legend: {...legend, selected: barData.selected},
+        series : [
+            {
+                name: 'Reloads',
+                type: 'pie',
+                radius : '55%',
+                center: ['40%', '50%'],
+                data: barData.seriesData,
+                itemStyle: {
+                    emphasis: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ],
+        toolbox,
+    };
+    const options = {
+        bar: optionsBar,
+        pie: optionsPie
+    };
+
+    let currentOption = 'bar'
     // use configuration item and data specified to show chart
-    myChart.setOption(option);
+    myChart.setOption(options[currentOption]);
+    function toggleChart() {
+        if (currentOption == 'bar') {
+            currentOption = 'pie'
+        } else if (currentOption == 'pie') {
+            currentOption = 'bar'
+        }
+        myChart.clear();
+        myChart.setOption(options[currentOption]);
+    }
+    document.getElementById('toggle-chart').addEventListener('click', toggleChart)
 })
