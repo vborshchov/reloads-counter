0cd1cb72ade1af688c4c1336a76eef4e362c32fe
Victor Borshchov <victor.borshchov@gmail.com>
2019-03-12 18:37:26 +0200

    add info about Navigation Timing API

--- src/js/popup/App.jsx
+++ src/js/popup/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import icon from "../../img/icon128.png"
+import timingInfo from "../../img/timing-overview.png"
 import Header from "./Header.jsx";
 import { hot } from "react-hot-loader";
+import "../../css/app.css";
 
 chrome.storage.local.get(["reloadStats"], function(result) {
     if (result.reloadStats) {
@@ -12,12 +13,23 @@ chrome.storage.local.get(["reloadStats"], function(result) {
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <Header name="Page reloads statistics!" />
-        <p>Hello, find me on src/js/popup/greeting_component.jsx</p>
-        <img src={icon} />
-      </div>
-    )
+        <div>
+            <Header name="Page reloads statistics!" />
+            <h2>
+                Navigation Timing
+            </h2>
+            <p>
+                <a href="http://www.w3.org/TR/navigation-timing/">Navigation Timing</a> gives access to timing
+                and navigation information for the overall document. Thus
+                one can easily calculate where time is spent between the
+                start of the navigation in the user agent until the end of
+                the load event processing. The Web application can access
+                timings for unloading the previous document, various HTTP
+                redirects, domain lookup, connection to the server, etc.:
+            </p>
+            <img className="timing-info" src={timingInfo} />
+        </div>
+    );
   }
 };
 
