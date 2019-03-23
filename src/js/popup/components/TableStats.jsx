2ae3a7c3d7ebe99f0177a883c296f1c90d32db9b
Victor Borshchov <victor.borshchov@gmail.com>
2019-03-24 00:10:44 +0200

    add styles for stats table

--- src/js/popup/components/TableStats.jsx
+++ src/js/popup/components/TableStats.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import chromeManager from "../../utils/chromeManager";
 import statsTransformer from "../../utils/statsTransformer";
+import "Styles/tableStats.scss";
 
 const TableStats = () => {
   const [statsData, setStatsData] = useState({ reloadStats: [] });
@@ -13,29 +14,38 @@ const TableStats = () => {
 
   return (
     <div className="table-stats">
-      <table>
-        <thead>
-          <tr>
-            <td>Host</td>
-            <td>Avarage load time</td>
-          </tr>
-        </thead>
-        <tbody>
-          {statsData &&
-            statsTransformer
-              .avarageLoadTimePerHost(statsData)
-              .sort((a, b) => b.value - a.value)
-              .map(obj => {
-                return (
-                  <tr key={obj.host}>
-                    <td>{obj.host}</td>
-                    <td>{obj.value.toFixed(2)} ms</td>
-                  </tr>
-                );
-              })
-          }
-        </tbody>
-      </table>
+      <div class="table-stats__header">
+        <table cellpadding="0" cellspacing="0" border="0">
+          <thead>
+            <tr>
+              <th className="table-stats__header-cell">Host</th>
+              <th className="table-stats__header-cell">Avarage load time</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div class="table-stats__content">
+        <table cellpadding="0" cellspacing="0" border="0">
+          <tbody>
+            {statsData &&
+              statsTransformer
+                .avarageLoadTimePerHost(statsData)
+                .sort((a, b) => b.value - a.value)
+                .map(obj => {
+                  return (
+                    <tr key={obj.host}>
+                      <td className="table-stats__cell">
+                        {obj.host}
+                      </td>
+                      <td className="table-stats__cell">
+                        {obj.value.toFixed(2)} ms
+                      </td>
+                    </tr>
+                  );
+                })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
